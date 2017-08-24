e1afc0686e4c4aabce8160c82a79414ff131bb9b
alejo <alejodaraio@gmail.com>
2017-08-24 14:48:33 -0300

    added test demo

---
+++ test/ClientTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace genoaTest;
+
+
+use PHPUnit_Framework_TestCase;
+
+class Client extends PHPUnit_Framework_TestCase{
+
+  /**
+   * This is a demo test, must be removed
+   */
+  public function testDemo() {
+    $this->assertEquals(true,  true);
+  }
+}
