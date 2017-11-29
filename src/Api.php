98f113e8ce9031617a0e923dbb8205c6c4396bd0
Augusto Franzoia <ajfranzoia@gmail.com>
2017-11-29 14:47:49 -0300

    fix: disable verbose cURL output by default

--- src/Api.php
+++ src/Api.php
@@ -120,8 +120,6 @@ class Api {
     }
 
     curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_VERBOSE, 1);
     curl_setopt($ch, CURLOPT_HTTPHEADER, $this->headers);
 
     $result = curl_exec($ch);
